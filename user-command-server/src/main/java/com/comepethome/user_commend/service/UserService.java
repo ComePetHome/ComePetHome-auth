91b4fafe637f74ff26e68bf0ef692519344d8cf8
mamagic <rrhhww0213@naver.com>
2024-01-01 16:16:30 +0900

    user 서버 분리

--- user-command-server/src/main/java/com/comepethome/user_commend/service/UserService.java
+++ user-command-server/src/main/java/com/comepethome/user_commend/service/UserService.java
@@ -6,6 +6,7 @@ import com.comepethome.user_commend.exception.user.UserAlreadyExistException;
 import com.comepethome.user_commend.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -18,8 +19,7 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    @Autowired
-    private PasswordEncoder encoder;
+    private final PasswordEncoder encoder = new BCryptPasswordEncoder();
 
     @Transactional
     public void save(UserDTO userDTO){
