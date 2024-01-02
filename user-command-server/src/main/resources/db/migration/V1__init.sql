7d77762d473494511ba7e1a525777ef99bb9c631
mamagic <rrhhww0213@naver.com>
2024-01-02 16:33:37 +0900

    mariaDb 설정

--- user-command-server/src/main/resources/db/migration/V1__init.sql
+++ user-command-server/src/main/resources/db/migration/V1__init.sql
@@ -1,16 +1,15 @@
-create table users(
-    id bigint AUTO_INCREMENT,
-    user_id varchar(255),
-    password varchar(255),
-    access_token varchar(255),
-    refresh_token varchar(255),
-    nick_name varchar(100),
-    image_url varchar(255),
-    create_at date,
-    update_at date,
-    name varchar(255),
-    phone_number varchar(255),
-    primary key (id)
+CREATE TABLE users (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    user_id VARCHAR(255),
+    password VARCHAR(255),
+    access_token VARCHAR(255),
+    refresh_token VARCHAR(255),
+    nick_name VARCHAR(255),
+    image_url VARCHAR(255),
+    create_at DATE,
+    update_at DATE,
+    name VARCHAR(255),
+    phone_number VARCHAR(255)
 );
 
 insert into users (user_id, password, nick_name, name, phone_number) values ('ryu', '1234', '닉네임', '류현우', '010-1234-1234');
