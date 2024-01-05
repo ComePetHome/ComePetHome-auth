d48597610c9ad5c726097457ea17cff506c924f7
JooHui-void <elis100@naver.com>
2024-01-06 01:58:57 +0900

    feat: noAuth 필터에서 토큰 내 userId 꺼내 헤더에 추가

--- gateway-server/src/main/java/com/comepethome/gateway/filter/NoAuthenticationFilter.java
+++ gateway-server/src/main/java/com/comepethome/gateway/filter/NoAuthenticationFilter.java
@@ -1,16 +1,18 @@
 package com.comepethome.gateway.filter;
 
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.comepethome.gateway.jwt.JwtService;
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
-import reactor.core.publisher.Mono;
-
 import java.util.Optional;
 
+@Slf4j
 @Component
 public class NoAuthenticationFilter extends AbstractGatewayFilterFactory<NoAuthenticationFilter.Config> {
     private final static String AUTH_ID = "Auth-Id";
@@ -18,7 +20,6 @@ public class NoAuthenticationFilter extends AbstractGatewayFilterFactory<NoAuthe
     private static final String REFRESH_TOKEN_SUBJECT = "refresh-token";
     @Autowired
     private final JwtService jwtService;
-
     @Autowired
     public NoAuthenticationFilter(JwtService jwtService){
         super(NoAuthenticationFilter.Config.class);
@@ -27,8 +28,20 @@ public class NoAuthenticationFilter extends AbstractGatewayFilterFactory<NoAuthe
 
     @Override
     public GatewayFilter apply(Config config) {
-        return (exchange, chain) -> chain.filter(exchange).then(Mono.fromRunnable(()->{
+        return (exchange, chain) ->{
                 ServerHttpResponse response = exchange.getResponse();
+                ServerHttpRequest request = exchange.getRequest();
+
+                //토큰에서 userId 꺼내서 헤더에 넣는다
+                Optional.ofNullable(request.getHeaders().getFirst("Authorization")).ifPresent(token-> {
+                    try {
+                        String userId = jwtService.getUserIdfromToken(token);
+                        request.mutate().header("userId", userId).build();
+                    } catch (JWTVerificationException e) {
+                        log.error("JWT Verification failed: {}", e.getMessage());
+                    }
+                });
+
                 Optional.ofNullable(response.getHeaders().getFirst(AUTH_ID)).ifPresent(userId -> {
 
                     String accessToken = jwtService.createAccessTokenWithPrefix(userId);
@@ -37,7 +50,8 @@ public class NoAuthenticationFilter extends AbstractGatewayFilterFactory<NoAuthe
                     response.getHeaders().add(ACCESS_TOKEN_SUBJECT, accessToken);
                     response.getHeaders().add(REFRESH_TOKEN_SUBJECT, refreshToken);
             });
-        }));
+         return chain.filter(exchange);
+        };
     }
 
     @Data
