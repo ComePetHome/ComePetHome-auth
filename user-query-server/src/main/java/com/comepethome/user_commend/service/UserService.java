536477a272b56695764b3d30a65d28db46a8a043
mamagic <rrhhww0213@naver.com>
2024-01-06 11:01:54 +0900

    user-query-server 수정

--- user-query-server/src/main/java/com/comepethome/user_commend/service/UserService.java
+++ user-query-server/src/main/java/com/comepethome/user_commend/service/UserService.java
@@ -4,7 +4,6 @@ import com.comepethome.user_commend.entity.User;
 import com.comepethome.user_commend.dto.UserDTO;
 import com.comepethome.user_commend.exception.user.UserNotExistException;
 import com.comepethome.user_commend.repository.UserRepository;
-import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,14 +16,12 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    @Transactional
     public String findUserIdByNameAndPhoneNumber(UserDTO userDTO){
         return Optional.ofNullable(userRepository.findByNameAndPhoneNumber(userDTO.getName(), userDTO.getPhoneNumber()))
                 .map(User::getUserId)
                 .orElseThrow(UserNotExistException::new);
     }
 
-    @Transactional
     public Optional<User> findByUser(String userId){
         return Optional.ofNullable(userRepository.findByUserId(userId));
     }
