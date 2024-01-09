24709b240688fae93ab92d66268216297e2b7c94
mamagic <rrhhww0213@naver.com>
2024-01-09 14:18:34 +0900

    user-command-server request @notblank 추가

--- user-command-server/src/main/java/com/comepethome/user_query/controller/UserController.java
+++ user-command-server/src/main/java/com/comepethome/user_query/controller/UserController.java
@@ -7,6 +7,7 @@ import com.comepethome.user_query.exception.ApiException;
 import com.comepethome.user_query.exception.ApiExceptionHandler;
 import com.comepethome.user_query.exception.SuccessResponseMessage;
 import com.comepethome.user_query.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/user")
--- user-command-server/src/main/java/com/comepethome/user_query/controller/request/UserJoinRequest.java
+++ user-command-server/src/main/java/com/comepethome/user_query/controller/request/UserJoinRequest.java
@@ -1,8 +1,8 @@
 package com.comepethome.user_query.controller.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
-import javax.validation.constraints.NotBlank;
 
 @Data
 public class UserJoinRequest {
