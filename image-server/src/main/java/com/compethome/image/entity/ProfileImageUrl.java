77cf447a127802012bfa9cd4e1323395898316c6
mamagic <rrhhww0213@naver.com>
2024-01-16 08:13:28 +0900

    image server 설정

--- image-server/src/main/java/com/compethome/image/entity/ProfileImageUrl.java
+++ image-server/src/main/java/com/compethome/image/entity/ProfileImageUrl.java
@@ -6,7 +6,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
-@Document(collection = "users")
+@Document(collection = "image_profile")
 @AllArgsConstructor
 public class ProfileImageUrl {
 
