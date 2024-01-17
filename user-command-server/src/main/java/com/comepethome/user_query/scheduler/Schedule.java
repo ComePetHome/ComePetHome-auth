786aac840ae35a071e4dca437a67f3217d01da16
mamagic <rrhhww0213@naver.com>
2024-01-17 14:52:45 +0900

    user-command-server schedule 셋팅

--- user-command-server/src/main/java/com/comepethome/user_query/scheduler/Schedule.java
+++ user-command-server/src/main/java/com/comepethome/user_query/scheduler/Schedule.java
@@ -6,13 +6,14 @@ import com.comepethome.user_query.controller.request.UserProfileUpdateRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 @Slf4j
 @Component
 @EnableAsync
+@EnableScheduling
 public class Schedule {
 
     @Value("${test.id}")
@@ -25,10 +26,11 @@ public class Schedule {
     private UserController userController;
     @Scheduled(fixedDelay = 1000)
     public void join() throws InterruptedException {
+        log.info("join update delete scheduled start - {}", System.currentTimeMillis() / 1000);
         userController.join(new UserJoinRequest(testId, testPw, "t1", "t1", "1"));
         userController.update(new UserProfileUpdateRequest("t","","t","0"), testId);
         userController.delete(testId);
-        log.info("join update delete finished - {}", System.currentTimeMillis() / 1000);
+        log.info("join update delete scheduled finished - {}", System.currentTimeMillis() / 1000);
         Thread.sleep(5000);
     }
 }
