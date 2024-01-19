047ef12efc00b8396adb67bbf587c264b5ddb964
mamagic <rrhhww0213@naver.com>
2024-01-19 16:41:47 +0900

    image server 재샛팅

--- image-server/src/main/java/com/compethome/image/ImageApplication.java
+++ image-server/src/main/java/com/compethome/image/ImageApplication.java
@@ -1,7 +1,9 @@
 package com.compethome.image;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ImageApplication {
@@ -9,5 +11,4 @@ public class ImageApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ImageApplication.class, args);
 	}
-
 }
